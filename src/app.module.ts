78e1ca36226366fef6e3d89f91ed039ea93f2ddb
ppereira <pablopereira27@gmail.com>
2022-11-13 22:43:45 -0300

    Gerando Resource REST para o Objeto Room
    $ nest g resource rooms

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { RoomsModule } from './rooms/rooms.module';
+
 @Module({
-  imports: [],
+  imports: [RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/rooms/rooms.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Room } from './entities/room.entity';
+
+import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
+
+@Injectable()
+export class RoomsService {
+    create(createRoomDto: CreateRoomDto) {
+        return 'This action adds a new room';
+    }
+
+    findAll() {
+        return `This action returns all rooms`;
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} room`;
+    }
+
+    update(id: number, updateRoomDto: UpdateRoomDto) {
+        return `This action updates a #${id} room`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} room`;
+    }
+}
