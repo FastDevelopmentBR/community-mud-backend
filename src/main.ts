b56dcf72f14c596e28d4273ae349cbc4dcda1c1c
ppereira <pablopereira27@gmail.com>
2022-12-10 14:04:37 -0300

    Adicionada variável GAME_URL

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const corsConfig = {
+    origin: [process.env.API_URL],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: [
+        'Accept',
+        'Content-Type',
+        'Authorization',
+    ]
+}
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
-        cors: {
-            origin: [
-                'http://localhost:4200',
-                'http://api.communitymud.fastdevelopment.com.br',
-                'https://api.communitymud.fastdevelopment.com.br'
-            ],
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-            preflightContinue: false,
-            optionsSuccessStatus: 204,
-            credentials: true,
-            allowedHeaders: [
-                'Accept',
-                'Content-Type',
-                'Authorization',
-            ]
-        },
+        cors: corsConfig,
         logger: ['error', 'warn'],
     });
 
