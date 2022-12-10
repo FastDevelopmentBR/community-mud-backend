95e078a57d88baa2c619f398050d3002a165f11e
ppereira <pablopereira27@gmail.com>
2022-12-10 14:11:28 -0300

    Refatorando classe de configurações de banco de dados.

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,16 @@
-export default () => ({
-    database: {
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+export default () => {
+    var databaseOptions: MysqlConnectionOptions = {
+        type: "mysql",
         host: process.env.DATABASE_HOST || 'localhost',
         port: parseInt(process.env.DATABASE_PORT) || 3306,
         username: process.env.DATABASE_USERNAME || 'root',
         password: process.env.DATABASE_PASSWORD,
-        synchronize: process.env.DATABASE_SYNC || false
+        synchronize: (process.env.DATABASE_SYNC == "true") ? true : false || false
     }
-});
+
+    return {
+        database: databaseOptions
+    }
+};
